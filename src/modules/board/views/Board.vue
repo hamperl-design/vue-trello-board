db72644392d161a8e2a7759f9ca39c38692208e4
Klaus Hamperl <info@hamperl-design.de>
2023-04-10 20:29:21 +0200

    add types and structure

---
+++ src/modules/board/views/Board.vue
@@ -0,0 +1,11 @@
+
+<script lang="ts" setup="">
+</script>
+
+<template>
+  <h1>Board.vue</h1>
+</template>
+
+<style lang='scss'>
+
+</style>
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type ID = string
+export interface Column {
+    title: string;
+    id: ID;
+    tasks: Task[];
+}
+export interface Task {
+    id: ID;
+    title: string;
+    createdAt: Date;
+}
